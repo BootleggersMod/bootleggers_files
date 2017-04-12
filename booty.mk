4be37c303bdff694e3d28bb4289c523762a025af
ElDainosor <eldainosor@gmail.com>
2017-04-12 07:04:39 +0000

    using just shishustuff

--- booty.mk
+++ booty.mk
@@ -24,9 +24,6 @@ endif
 
 ## Set variables of folder, because it's less cluttered.
 
-# This is where all the sounds belong
-SHISHUSOUNDS_PATH := vendor/bootleggers/prebuilt/Sounds
-
 # This is where all the apps files belong
 SHISHUSTUFF_PATH := vendor/bootleggers/prebuilt
 
@@ -88,25 +85,25 @@ endif
 ## Set the Ringtone part of the mod
 # Ringtone files
 PRODUCT_COPY_FILES += \
-    $(SHISHUSOUNDS_PATH)/Frost.ogg:system/media/audio/ringtones/Frost.ogg \
-    $(SHISHUSOUNDS_PATH)/Beauty_pub.ogg:system/media/audio/ringtones/Beauty_pub.ogg \
-    $(SHISHUSOUNDS_PATH)/Myst.ogg:system/media/audio/ringtones/Myst.ogg
+    $(SHISHUSTUFF_PATH)/Sounds/Frost.ogg:system/media/audio/ringtones/Frost.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Beauty_pub.ogg:system/media/audio/ringtones/Beauty_pub.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Myst.ogg:system/media/audio/ringtones/Myst.ogg
     
 # Notifications files
 PRODUCT_COPY_FILES += \
-    $(SHISHUSOUNDS_PATH)/Nights.ogg:system/media/audio/notifications/Nights.ogg \
-    $(SHISHUSOUNDS_PATH)/Button.ogg:system/media/audio/notifications/Button.ogg \
-    $(SHISHUSOUNDS_PATH)/Chimes.wav:system/media/audio/notifications/Chimes.wav \
-    $(SHISHUSOUNDS_PATH)/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
-    $(SHISHUSOUNDS_PATH)/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
-    $(SHISHUSOUNDS_PATH)/Redmond.wav:system/media/audio/notifications/Redmond.wav \
-    $(SHISHUSOUNDS_PATH)/Talitha.ogg:system/media/audio/notifications/Talitha.ogg
+    $(SHISHUSTUFF_PATH)/Sounds/Nights.ogg:system/media/audio/notifications/Nights.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Button.ogg:system/media/audio/notifications/Button.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Chimes.wav:system/media/audio/notifications/Chimes.wav \
+    $(SHISHUSTUFF_PATH)/Sounds/CyanMail.ogg:system/media/audio/notifications/CyanMail.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/CyanMessage.ogg:system/media/audio/notifications/CyanMessage.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Redmond.wav:system/media/audio/notifications/Redmond.wav \
+    $(SHISHUSTUFF_PATH)/Sounds/Talitha.ogg:system/media/audio/notifications/Talitha.ogg
     
     
 # Alarms files
 PRODUCT_COPY_FILES += \
-    $(SHISHUSOUNDS_PATH)/LikeWhat.ogg:system/media/audio/alarms/LikeWhat.ogg \
-    $(SHISHUSOUNDS_PATH)/Shadow.ogg:system/media/audio/alarms/Shadow.ogg
+    $(SHISHUSTUFF_PATH)/Sounds/LikeWhat.ogg:system/media/audio/alarms/LikeWhat.ogg \
+    $(SHISHUSTUFF_PATH)/Sounds/Shadow.ogg:system/media/audio/alarms/Shadow.ogg
     
 #Set the new custom sounds
 PRODUCT_PROPERTY_OVERRIDES += \
